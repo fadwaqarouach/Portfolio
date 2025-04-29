9af00481250a02f5a77e889f34cdee893ff4a888
fadwaqarouach <fqarouach@gmail.com>
2025-04-29 10:44:23 +0200

    Déploiement sur gh-pages

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { Gem, Facebook, Twitter, Youtube } from "lucide-react"
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center  p-10">
+            <aside>
+
+                <Gem className="w-10 h-10" />
+                <p className="font-bold">
+                    Fadwa
+                    <span className="text-secondary">QAROUACH</span>
+                </p>
+                <p>Copyright © {new Date().getFullYear()} -  Tous droits réservés</p>
+            </aside>
+            <nav>
+                <div className="grid grid-flow-col gap-4">
+                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <Twitter className="w-6 h-6 text-current" />
+                    </a>
+                    <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                        <Youtube className="w-6 h-6 text-current" />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer">
+                        <Facebook className="w-6 h-6 text-current" />
+                    </a>
+                </div>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer
